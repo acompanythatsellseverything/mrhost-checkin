623f23cd77480758bfbceca0978eb03b1a912530
Darii <i553730@fontysict.nl>
2025-07-15 19:21:20 +0200

    demo changes

--- Hostaway/app/script/script.py
+++ Hostaway/app/script/script.py
@@ -62,7 +62,7 @@ def schedule_jobs():
         visit_registration_endpoint,
         'cron',
         hour='10,13,19',
-        minute='0,19',
+        minute='0,23',
         timezone=spain_tz,
         id="visit_registration",
         replace_existing=True
@@ -73,7 +73,7 @@ def schedule_jobs():
         visit_verification_endpoint,
         'cron',
         hour='10,13,19',
-        minute='0,20',
+        minute='0,24',
         timezone=spain_tz,
         id="visit_verification",
         replace_existing=True
